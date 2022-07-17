aeee573beb466c183750ab8978a50c8a8464ca45
Davyd Khanin <31155164+Davidoskin@users.noreply.github.com>
2022-07-17 22:40:09 +0100

    fully redone app. Now this app is on russian and helps in sports betting

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import eel
+eel.init('web')
 
-eel.init("web")
 
-eel.start("main.html", size=(800, 600))
+@eel.expose
+def dogon_calculator(win, lose, outcome):
+    res = (float(win) + float(lose)) / (float(outcome) - 1)
+    return "Следующая ставка: " + str(res)
+
+
+eel.start('main.html', size=(800, 600))
